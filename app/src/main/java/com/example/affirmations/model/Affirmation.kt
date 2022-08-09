c97e40b03bb5a0aaa4842c38344da465c2b2ca12
25Vaishnavibarge17 <vaishnavibarge2510@gmail.com>
2022-08-09 17:42:31 +0530

    Final Commit

---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,6 @@
+package com.example.affirmations.model
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+data class Affirmation(@DrawableRes val stringResourceId:Int,@StringRes val imageResourceId:Int) {
+
+}
